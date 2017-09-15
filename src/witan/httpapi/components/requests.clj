fd928c7664d15ce8bed41d9adfb0f21cf81b4033
Antony Woods <antony@mastodonc.com>
2017-09-15 17:11:28 +0100

    Manually parse transit bodies

--- src/witan/httpapi/components/requests.clj
+++ src/witan/httpapi/components/requests.clj
@@ -16,22 +16,29 @@
   [opts]
   (merge {:content-type :transit+json
           :accept :transit+json
+          ;;:as :transit+json ;; don't let aleph try to decode, it breaks with empty bodies
           :throw-exceptions false}
          opts))
+
+(defn parse-body
+  [^java.io.ByteArrayInputStream body]
+  (when (and body (pos? (.available body)))
+    (let [x (parse-transit body :json {})]
+      #_(log/debug "DECODED BODY" x)
+      x)))
 
 (defrecord HttpRequester [directory]
   Request
   (GET [this service route opts]
     (let [full-route (build-route (get directory service) route)]
       (log/debug "GET request sent to" service full-route)
-      (let [{:keys [body status]} @(http/get full-route (add-default-opts opts))
-            body (if body (parse-transit body :json {}) body)]
-        [status body])))
+      (let [{:keys [body status]} @(http/get full-route (add-default-opts opts))]
+        [status (parse-body body)])))
   (POST [this service route opts]
     (let [full-route (build-route (get directory service) route)]
       (log/debug "POST request sent to" service full-route)
       (let [{:keys [body status]} @(http/post full-route (add-default-opts opts))]
-        [status body])))
+        [status (parse-body body)])))
 
   component/Lifecycle
   (start [component]
