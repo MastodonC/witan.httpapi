d55c4bce954b5a52f5f571861a16b532d510bd04
Antony Woods <antony@mastodonc.com>
2017-09-18 15:23:00 +0100

    WIP fixing tests

--- test/witan/httpapi/components/auth_test.clj
+++ test/witan/httpapi/components/auth_test.clj
@@ -2,6 +2,7 @@
   (:require [witan.httpapi.components.auth :as auth :refer :all]
             [com.stuartsierra.component :as component]
             [clojure.test :refer :all]
+            [witan.httpapi.test-base :refer :all]
             [witan.httpapi.mocks :as mocks]
             [witan.httpapi.system :as sys]
             [clj-time.core :as t]
@@ -9,36 +10,41 @@
 
 (def sys (atom nil))
 
-(def profile (keyword (env :system-profile "test")))
+(defn test-system
+  [sys-fn]
+  (with-redefs [sys/new-requester (fn [config] (mocks/->MockRequester))]
+    (sys-fn)))
 
 (defn start-system
   [all-tests]
-  (reset! sys (component/start
-               (with-redefs [sys/new-requester (fn [config] (mocks/->MockRequester))]
-                 (sys/new-system profile))))
-  (all-tests)
-  (component/stop @sys)
-  (reset! sys nil))
+  (let [mocks-fn (if (= :test profile)
+                   std-system
+                   test-system)]
+    (reset! sys (component/start
+                 (mocks-fn
+                  #(sys/new-system profile))))
+    (all-tests)
+    (component/stop @sys)
+    (reset! sys nil)))
 
 (defn auth
   []
   (:auth @sys))
 
-(def test-auth-token
-  "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InRlc3RAbWFzdG9kb25jLmNvbSIsImlkIjoiMDcwYjM3MTAtMjJiZS00ZDczLWJmY2YtZGM2MWMyM2RlNzgxIiwibmFtZSI6IlRlc3QgVXNlciIsInVzZXItZ3JvdXBzIjpbImMwZDVhOThjLTIwMTEtNGI3ZC04Y2QzLWI4MzljMDczNDQzMSJdLCJzZWxmLWdyb3VwIjoiZGRkMzhlYTAtOGI5MC00Yzk5LWIyZWYtNTRhZGQ2MjFiMWUyIiwiZXhwIjoxNTA0NjExMzgxMzc5fQ.lnbQble-xVhTMCCgJdV-EXCNwJqGOVKL4NRdFS8FfjUa4982iYDp-CYYl389l7xjXjn0CRxHfNEdObhg9KCkYac8USUdD0f8BrDorP_4de7BQy-kXVu214sauTpm0yyzK1Aygmz3rGV2u3_ZDJBlpQG4wmIiHQcp8T-tDNkvDyMJYQ8QVdit1dxvj1inTeBdZ2VavLWAKraK7gw7YHDfBiLLA_J2MCbevQHCaYgqKy9nV2G9yE9C1mSJZ143w8yTV1c5TdnXatxbYKPtOapWu_7VybpiMVrLl-suV2tEoJmAh-Kya2UlwqCqoHbNVgwXpEGHSCmoiyQSc5lR4GNyQQ")
-
 (use-fixtures :once start-system)
 
 (deftest login-test
-  (let [r (auth/login (auth) "test@mastodonc.com" "Secret123")]
-    (is (= [:post :heimdall "/create-auth-token"] r))))
+  (let [[s r] (auth/login (auth) "test@mastodonc.com" "Secret123")]
+    (is (contains? r :token-pair))
+    (is (contains? (:token-pair r) :auth-token))
+    (is (contains? (:token-pair r) :refresh-token))))
 
 (deftest refresh-test
   (let [r (auth/refresh (auth) {})]
     (is (= [:post :heimdall "/refresh-auth-token"] r))))
 
-(deftest authenticate-test
-  (let [r (auth/authenticate (auth) (t/date-time 0) test-auth-token)]
-    (is (= #:kixi.user{:id "070b3710-22be-4d73-bfcf-dc61c23de781",
-                       :groups ["ddd38ea0-8b90-4c99-b2ef-54add621b1e2" "c0d5a98c-2011-4b7d-8cd3-b839c0734431"]
-                       :self-group "ddd38ea0-8b90-4c99-b2ef-54add621b1e2"} r))))
+#_(deftest authenticate-test
+    (let [r (auth/authenticate (auth) (t/date-time 0) test-auth-token)]
+      (is (= #:kixi.user{:id "070b3710-22be-4d73-bfcf-dc61c23de781",
+                         :groups ["ddd38ea0-8b90-4c99-b2ef-54add621b1e2" "c0d5a98c-2011-4b7d-8cd3-b839c0734431"]
+                         :self-group "ddd38ea0-8b90-4c99-b2ef-54add621b1e2"} r))))
