b9e798bb80d4ebd881175fc44b42aa636d16ea31
Antony Woods <antony@mastodonc.com>
2017-10-23 11:22:49 +0100

    Now we upgraded CA we can go back to this

--- src/witan/httpapi/api.clj
+++ src/witan/httpapi/api.clj
@@ -63,13 +63,7 @@
 (def not-found-routes
   (let [not-found-resp (not-found "Not Found")]
     (context "/" []
-             (GET "/*" [] not-found-resp)
-             (HEAD "/*" [] not-found-resp)
-             (PATCH "/*" [] not-found-resp)
-             (DELETE "/*" [] not-found-resp)
-             (OPTIONS "/*" [] not-found-resp)
-             (POST "/*" [] not-found-resp)
-             (PUT "/*" [] not-found-resp))))
+             (ANY "/*" [] not-found-resp))))
 
 (def auth-routes
   (context "/api" []
